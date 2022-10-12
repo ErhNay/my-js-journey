60a5c56b2740b2a94abc7329d100410ff273f9cb
ErhNay <erhnay86@gmail.com>
2022-10-12 02:55:45 +0000

    Add some codes

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ![js-1665321530781-8314](https://user-images.githubusercontent.com/109716271/194759079-73cab7d0-39fa-4cca-b007-4f3f28945771.jpg)
 # My JavaScript Journey
-This repo contains codes that helped with my JavaScript journey from the fundamentals through to some advanced concepts 
+
+## This repo contains codes that helped with my JavaScript journey from the fundamentals through to some advanced concepts 
+
+-- Will keep updating this repo from time to time. Thank you ☺☺☺☺☺☺☺
--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,1 +1,65 @@
-alert('hdhdh')
+const defaultValue = 0;
+
+let currentResult = defaultValue;
+
+const logEntries = [];
+
+//gets user input form an input field
+function getUserInput() {
+  return +userInput.value;
+}
+
+//Creates and outputs Calculation log
+function createAndWriteOutput(operator, resultBeforeCal, calNum) {
+  const calcDesc = `${resultBeforeCal} ${operator} ${calNum}`;
+  outputResult(currentResult, calcDesc); // from the vendor file
+}
+
+//logging the entry
+
+function writeToLog(opIdentifier, prevValue, numberEntered, newResult) {
+  const logEntry = {
+    opIdentifier,
+    prevValue,
+    numberEntered,
+    newResult,
+  };
+
+  logEntries.push(logEntry);
+  console.log(logEntries);
+}
+
+function add() {
+  const receivedValue = getUserInput();
+  const initialValue = currentResult;
+  currentResult = currentResult + receivedValue;
+  createAndWriteOutput('+', initialValue, receivedValue);
+  writeToLog('Add', initialValue, receivedValue, currentResult);
+}
+
+function subtract() {
+  const receivedValue = getUserInput();
+  const initialValue = currentResult;
+  currentResult = currentResult - receivedValue;
+  createAndWriteOutput('+', initialValue, receivedValue);
+  writeToLog('Subtract', initialValue, receivedValue, currentResult);
+}
+function multiply() {
+  const receivedValue = getUserInput();
+  const initialValue = currentResult;
+  currentResult = currentResult * receivedValue;
+  createAndWriteOutput('*', initialValue, receivedValue);
+  writeToLog('Multiply', initialValue, receivedValue, currentResult);
+}
+function divide() {
+  const receivedValue = getUserInput();
+  const initialValue = currentResult;
+  currentResult = currentResult / receivedValue;
+  createAndWriteOutput('/', initialValue, receivedValue);
+  writeToLog('Divide', initialValue, receivedValue, currentResult);
+}
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
